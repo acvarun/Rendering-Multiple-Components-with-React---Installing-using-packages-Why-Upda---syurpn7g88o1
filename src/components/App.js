293e141c357fe8d2bef019c236ff2b15a33fc3c4
acvarun <96919265+acvarun@users.noreply.github.com>
2022-12-15 23:02:07 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,8 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="main">
-      <h1 data-ns-test attribute="project-name">name</h1>
-      <p data-ns-test attribute="project-description">description</p>
+      <h1 data-ns-test-attribute="project-name">name</h1>
+      <p data-ns-test-attribute="project-description">description</p>
     </div>
   )
 }
